b0ca35aacbf68e28fe4606ac1da983a595c26380
Andrey Dor <andrewdor992@gmail.com>
2021-11-19 16:51:37 +0300

    feat: add sass and normalize

---
+++ src/modules/core/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = function app(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">START</header>
+        </div>
+    );
+};
+
+export default App;
