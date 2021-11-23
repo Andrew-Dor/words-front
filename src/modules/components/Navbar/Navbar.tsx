e87b19fed16de290e47dda0723683b66d36c1eab
Andrey Dor <andrewdor992@gmail.com>
2021-11-23 17:26:32 +0300

    feat: add disabled style

--- src/modules/components/Navbar/Navbar.tsx
+++ src/modules/components/Navbar/Navbar.tsx
@@ -10,13 +10,14 @@ export const Navbar = (): JSX.Element => {
     return (
         <div className="navbar">
             <p>Nav</p>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: 480 }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', width: 580 }}>
                 <Button size="medium" text="Default" variant="primary" />
-                <Button size="medium" text="Default" variant="success" />
-                <Button size="medium" text="Default" variant="warning" />
-                <Button size="medium" text="Default" variant="error" />
+                <Button size="medium" text="Default" variant="success" disabled />
+                <Button size="medium" text="Default" variant="warning" disabled />
+                <Button size="medium" text="Default" variant="error" disabled />
                 <Button size="medium" text="Default" isSquare />
-                <Button size="medium" text="Default" outline />
+                <Button size="medium" text="Default" outline disabled />
+                <Button size="medium" text="Default" variant="primary" disabled />
             </div>
         </div>
     );
