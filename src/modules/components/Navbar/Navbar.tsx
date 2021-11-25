19224b0ef3f574ec22a41c1cd241860b0c250c46
Andrey Dor <andrewdor992@gmail.com>
2021-11-25 17:14:31 +0300

    fix: clear navbar component

--- src/modules/components/Navbar/Navbar.tsx
+++ src/modules/components/Navbar/Navbar.tsx
@@ -12,53 +12,6 @@ export const Navbar = (): JSX.Element => {
     return (
         <div className="navbar">
             <p>Nav</p>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: 980 }}>
-                <Button onClick={() => console.log('test')} size="medium" text="Default" variant="primary" />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" variant="success" />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" variant="warning" />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" variant="error" />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" isSquare />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" outline disabled />
-                <Button onClick={() => console.log('test')} size="medium" text="Default" variant="primary" disabled />
-                <Button
-                    onClick={() => console.log('test')}
-                    size="medium"
-                    text="Default"
-                    variant="primary"
-                    startIcon={<TiAdjustBrightness />}
-                />
-                <Button
-                    onClick={() => console.log('test')}
-                    size="medium"
-                    text="Default"
-                    variant="success"
-                    endIcon={<TiAdjustBrightness />}
-                />
-                <Button
-                    onClick={() => console.log('test')}
-                    size="medium"
-                    text=""
-                    variant="warning"
-                    endIcon={<IoIosAirplane />}
-                />
-                <Button
-                    onClick={() => console.log('test')}
-                    size="large"
-                    text=""
-                    variant="error"
-                    circle
-                    endIcon={<TiAdjustBrightness />}
-                />
-                <Button
-                    onClick={() => console.log('test')}
-                    size="medium"
-                    text="asdasdsad"
-                    variant="success"
-                    outline
-                    circle
-                    endIcon={<IoIosAirplane />}
-                />
-            </div>
         </div>
     );
 };
