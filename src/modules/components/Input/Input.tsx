035322de65fbe146e3416779c98c05c39096dd3e
Andrey Dor <andrewdor992@gmail.com>
2021-12-02 17:42:01 +0300

    feat: type password styling

---
+++ src/modules/components/Input/Input.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import classnames from 'classnames';
+import { MdVisibility, MdVisibilityOff } from 'react-icons/md';
+
+interface IInputProps {
+    value: string;
+    inline?: boolean;
+    placeholder?: string;
+    type?: React.HTMLInputTypeAttribute;
+    startIcon?: React.ReactElement;
+}
+
+export const Input = ({ value, type = 'text', inline, placeholder, startIcon }: IInputProps) => {
+    const classes = classnames(
+        {
+            input: true,
+        },
+        { inline },
+        { iconInput: !!startIcon },
+        { passwordInput: type === 'password' },
+    );
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [inputType, setInputType] = useState<React.HTMLInputTypeAttribute>('text');
+
+    useEffect(() => {
+        if (type === 'password') {
+            if (showPassword) {
+                setInputType('text');
+            } else {
+                setInputType('password');
+            }
+        } else {
+            setInputType(type);
+        }
+    }, [type, showPassword]);
+
+    return (
+        <div className="inputWrapper">
+            <div className="startIcon">{startIcon}</div>
+            <input type={inputType} className={classes} placeholder={placeholder} />
+            <span className="focusBorder" />
+            {type === 'password' &&
+                (showPassword ? (
+                    <MdVisibilityOff onClick={() => setShowPassword(false)} className="passwordIcon" />
+                ) : (
+                    <MdVisibility onClick={() => setShowPassword(true)} className="passwordIcon" />
+                ))}
+        </div>
+    );
+};
